72eccb15f8adf99e75b50996ce054cab84e22d83
gautamsharm29 <gautamsharm29@gmail.com>
2026-02-08 14:05:46 +0530

    Initial commit of Chamet codes

---
+++ im/zego/zegoexpress/entity/ZegoSwitchPlayingStreamConfig.java
@@ -0,0 +1,23 @@
+package im.zego.zegoexpress.entity;
+
+import android.app.Application;
+import android.graphics.*;
+import im.zego.zegoexpress.*;
+import im.zego.zegoexpress.callback.*;
+import im.zego.zegoexpress.constants.*;
+import im.zego.zegoexpress.entity.*;
+import im.zego.zegoexpress.internal.*;
+import java.nio.*;
+import java.util.*;
+import org.json.*;
+
+/**
+ * Extended parameters for the [switchPlayingStream] interface.
+ *
+ * Extended parameters for the [switchPlayingStream] interface.
+ */
+public class ZegoSwitchPlayingStreamConfig {
+
+    /** Switch playing stream type. */
+    public ZegoSwitchPlayingStreamType switchType = ZegoSwitchPlayingStreamType.DEFAULT;
+}
